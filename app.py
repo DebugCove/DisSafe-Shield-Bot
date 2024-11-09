2af3eb0ae9eee59b24a5ea99811f2fe1d813c9e2
LuisGomes18 <luiscgomes0@gmail.com>
2024-11-09 22:08:41 +0000

    Adding the ban command

--- app.py
+++ app.py
@@ -28,6 +28,26 @@ async def ping(interaction: discord.Interaction):
     )
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
+@bot.tree.command(name='ban', description='Ban a user')
+@app_commands.checks.has_permissions(ban_members=True)
+async def ban(interaction: discord.Interaction, member: discord.Member, reason: str):
+    try:
+        await member.ban(reason=reason)
+        embed = discord.Embed(
+            title='Ban',
+            description=f'{member} was banned. Reason: {reason}',
+            color=discord.Color.green()
+        )
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+    except Exception as error:
+        embed = discord.Embed(
+            title='Ban',
+            description=f'Error banning {member.mention}',
+            color=discord.Color.red()
+        )
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        print(f'There was an error banning the user {member} the reason would be {reason}\nError {error}')
+
 
 
 load_dotenv()
