16373d2519084452ac694880e1c86fd38bd8faf3
LuisGomes18 <luiscgomes0@gmail.com>
2025-01-29 16:56:10 +0000

    ✨ feat: Added event to bot that creates a json file containing server information

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import nextcord
 from nextcord import Interaction, ChannelType
 from nextcord.ext import commands
 from nextcord.ui import Button, View
+from config.config import create_config_file
+from config.config import load_config
 
 
 load_dotenv()
@@ -15,9 +17,34 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f'{bot.user} is online and ready to use!')
 
+@bot.event
+async def on_guild_join(guild):
+    config = load_config()
+    id_debug_cove = config.get('server_id')
+    id_channel_logs_debug_cove = config.get('chat_logs_id')
+
+    print(f"🎉 The bot entered the server: {guild.name} (ID: {guild.id})")
+    create_config_file(guild.id)
+
+    guild_debug_cove = bot.get_guild(int(id_debug_cove))
+    if guild_debug_cove:
+        log_channel_debug_cove = guild_debug_cove.get_channel(int(id_channel_logs_debug_cove))
+        if log_channel_debug_cove:
+            embed = nextcord.Embed(
+                title='Bot was added in new server'
+                description=(
+                    f'Bot was added to a new server\n'
+                    f'Server name: {guild.name}\n'
+                    f'Server ID: {guild.id}'
+                ),
+                color=nextcord.Color.blue()
+            )
+            await log_channel_debug_cove.send(embed=embed)
+
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'Ping {round(bot.latency * 1000)}ms')
+
 
 
 TOKEN = getenv('TOKEN')
