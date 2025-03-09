e748bc92f8cbd96717774ab2d400bb38830ad9da
LuisGomes18 <luiscgomes0@gmail.com>
2025-03-09 00:57:02 +0000

    ✨ feat: Addition of the report command, exception handling for bot execution and code formatting

--- app.py
+++ app.py
@@ -1,54 +1,85 @@
+import asyncio
+from datetime import datetime, timedelta, timezone
 from os import getenv
 from dotenv import load_dotenv
+
 import nextcord
 from nextcord import Interaction, ChannelType
-from nextcord.ext import commands
+from nextcord.ext import commands, tasks
 from nextcord.ui import Button, View
-from config.config import create_config_file
-from config.config import load_config
 
+from config.config import create_config_file
+from extras.logs import send_logs_guild, send_logs_debug
 
 load_dotenv()
 intents = nextcord.Intents.all()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+IP_API = getenv('API_URL')
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} is online and ready to use!')
 
+
 @bot.event
 async def on_guild_join(guild):
-    config = load_config()
-    id_debug_cove = config.get('server_id')
-    id_channel_logs_debug_cove = config.get('chat_logs_id')
-
-    print(f"🎉 The bot entered the server: {guild.name} (ID: {guild.id})")
+    print(f'\U0001F389 The bot entered the server: {guild.name} (ID: {guild.id})')
     create_config_file(guild.id)
 
-    guild_debug_cove = bot.get_guild(int(id_debug_cove))
-    if guild_debug_cove:
-        log_channel_debug_cove = guild_debug_cove.get_channel(int(id_channel_logs_debug_cove))
-        if log_channel_debug_cove:
-            embed = nextcord.Embed(
-                title='Bot was added in new server'
-                description=(
-                    f'Bot was added to a new server\n'
-                    f'Server name: {guild.name}\n'
-                    f'Server ID: {guild.id}'
-                ),
-                color=nextcord.Color.blue()
-            )
-            await log_channel_debug_cove.send(embed=embed)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'Ping {round(bot.latency * 1000)}ms')
+@bot.slash_command(description='See bot latency')
+async def ping(interaction: Interaction):
+    await interaction.response.send_message(f'Ping: {round(bot.latency * 1000)}ms', ephemeral=True)
 
 
+@bot.slash_command(description='Make a user report')
+async def make_report(
+    interaction: Interaction,
+    user: nextcord.User,
+    reason: str,
+    proof: str,
+    accuser: nextcord.User = None
+):
+    if user == interaction.user:
+        await interaction.response.send_message('You cannot report yourself.', ephemeral=True)
+        return
+    
+    if user == bot.user:
+        await interaction.response.send_message('You cannot report the bot.', ephemeral=True)
+        return
 
-TOKEN = getenv('TOKEN')
-if TOKEN is None:
-    raise Exception('TOKEN is not set')
+    accuser = accuser or interaction.user
+    guild = interaction.guild
+    if user.bot:
+        result_bot = 'Yes'
+    result_bot = 'No'
 
-bot.run(TOKEN)
+    embed = nextcord.Embed(
+        title='Report Sent',
+        description=(
+            f'**User:** {user.mention}\n'
+            f'**Reason:** {reason}\n'
+            f'**Proof:** {proof}\n'
+            f'**Bot:** {result_bot}\n'
+            f'**Accuser:** {accuser.mention}'
+        ),
+        color=nextcord.Color.green()
+    )
+    embed.set_footer(
+        text=f'Reported by: {interaction.user.display_name} | Server: {guild.id}',
+        icon_url=interaction.user.display_avatar.url
+    )
+
+    await send_logs_guild(interaction, bot, guild.id, embed)
+    await send_logs_debug(interaction, bot, embed)
+    await interaction.followup.send('Report made successfully!', ephemeral=True)
+
+
+try:
+    TOKEN = getenv('TOKEN')
+    if not TOKEN:
+        raise ValueError('TOKEN is not set.')
+    bot.run(TOKEN)
+except Exception as error:
+    print(f'Error starting bot: {error}')
