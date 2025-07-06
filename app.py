56ca44721ad9fb685ac4e6032d65765876d0326c
LuisGomes18 <luiscgomes0@gmail.com>
2025-07-06 13:50:11 +0100

    ♻️ refactor: Refactored main codebase

--- app.py
+++ app.py
@@ -1,43 +1,29 @@
-from nextcord import Interaction, ChannelType
-from nextcord.ext import commands, tasks
-from nextcord.ui import Button, View
-import nextcord
-
-from os import getenv
 from dotenv import load_dotenv
-from config.main import load_config
-import logging
-import logging.config
+from os import getenv
+from discord.ext import commands
+import discord
+import asyncio
 
 
 load_dotenv()
-logging.config.fileConfig('logging.conf')
-logger = logging.getLogger('logs_bot')
-config = load_config()
-prefix = config['service']['bot']['prefix']
-if prefix is None or not isinstance(prefix, str):
-    logger.info('Prefix not defined in settings use default "!"')
-    prefix = '!'
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+TOKEN = getenv('TOKEN')
 
-intents = nextcord.Intents.all()
-bot = commands.Bot(command_prefix=prefix, intents=intents)
+async def load_cogs():
+    await bot.load_extension('cogs.geral')
+    await bot.load_extension('cogs.events')
 
-@bot.event
-async def on_ready():
-    logger.info(f'Bot {bot.user} is online and ready to use!')  
-
-@bot.slash_command(description='See bot latency')
-async def ping(interaction: Interaction):
-    await interaction.response.defer(ephemeral=True)
-    await interaction.followup.send(f'Ping: {round(bot.latency * 1000)}ms', ephemeral=True)
+async def main():
+    async with bot:
+        await load_cogs()
+        await bot.start(TOKEN)
 
 
 try:
-    TOKEN = getenv('TOKEN')
-    if not TOKEN or not isinstance(TOKEN, str):
-        logger.error('token has not been defined or is invalid')
-        exit(1)
-    bot.run(TOKEN)
-except Exception as error:
-    logger.error(f'Error when trying to run the bot check if the token is invalid: {error}')
+    asyncio.run(main())
+except KeyboardInterrupt:
+    exit()
+except Exception as e:
+    print(f'An error occurred: {e}')
     exit(1)
